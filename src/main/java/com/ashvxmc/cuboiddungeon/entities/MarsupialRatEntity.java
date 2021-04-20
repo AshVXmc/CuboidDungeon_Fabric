f21776ff5a632977f85edda5e1a0f7b2015decf1
AshVXmc <52189250+Ashton2111@users.noreply.github.com>
2021-04-20 12:05:40 +0700

    make marsupial rats attack wolves

--- src/main/java/com/ashvxmc/cuboiddungeon/entities/MarsupialRatEntity.java
+++ src/main/java/com/ashvxmc/cuboiddungeon/entities/MarsupialRatEntity.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -60,7 +62,8 @@ public class MarsupialRatEntity extends PathAwareEntity implements IAnimatable {
     @Override
     protected void initGoals() {
         this.goalSelector.add(3,new MeleeAttackGoal(this,0.8f,false));
-        this.goalSelector.add(2, new FollowTargetGoal<>(this, PlayerEntity.class, true, true));
+        this.goalSelector.add(2, new FollowTargetGoal<>(this, PlayerEntity.class,true, true));
+        this.goalSelector.add(2,new FollowTargetGoal<>(this, WolfEntity.class,true,true));
         this.goalSelector.add(2, new LookAroundGoal(this));
     }
 
